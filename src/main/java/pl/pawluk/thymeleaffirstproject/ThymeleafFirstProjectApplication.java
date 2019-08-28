05d168aa157ee0f6d4e37375e016d0dc78be32d3
Piotr <piotr.pawlk@gmail.com>
2019-08-28 06:51:04 +0200

    added tasks

---
+++ src/main/java/pl/pawluk/thymeleaffirstproject/ThymeleafFirstProjectApplication.java
@@ -0,0 +1,13 @@
+package pl.pawluk.thymeleaffirstproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafFirstProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThymeleafFirstProjectApplication.class, args);
+    }
+
+}
